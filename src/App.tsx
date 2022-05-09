5a307dfa71f9ba72a51257183fbe2ed0c93a4b1d
Ido Leibovich <ido.leibole@gmail.com>
2022-05-09 19:30:42 +0300

    Run with code server

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import userflow from "userflow.js";
+
+userflow.init("ct_2cr2apkujvh7xag4ezserzr6ey");
+
+userflow.identify("USER_ID", {
+  name: "USER_NAME2",
+});
+
+const chmln = require("@chamaeleonidae/chmln");
+
+chmln.init(
+  "S4gtYPM542SFouBLvdWCKQAioJdNtV9siNSJ7u5lB44ITy-1NLunl-DCttayvd09l0ABhd"
+);
+
+chmln.identify("someid");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: "100vh" }}>
+      <iframe
+        src="http://localhost:8080?folder=/Users/idoleibovich/workspace/lightrun-code-server"
+        width="100%"
+        height="100%"
+        title="Code server"
+      />
     </div>
   );
 }
